7f5689a601b1212c183407d6a68c3f606a727455
Glitch (glitch-hello-react) <none>
2022-02-14 13:19:21 +0000

    📺🛫 Checkpoint ./src/app.jsx:8338587/403 ./watch.json:8338587/21

--- src/app.jsx
+++ src/app.jsx
@@ -8,11 +8,22 @@ import { roundTo } from "./logic/utils";
 
 import "./styles/styles.css";
 
+const DRINKS_KEY = "drinks";
+
 export default function Home() {
   const [drinks, setDrinks] = useState([
     { name: "Beer", amount: "Pint", percentage: 4.1 },
     { name: "Wine", amount: "125ml", percentage: 10 }
   ]);
+  
+  const storage = window.localStorage;
+  
+  useEffect(() => {
+    const savedDrinks = storage.getItem(DRINKS_KEY);
+    if (!savedDrinks) return;
+    
+    const parsed = 
+  }, []);
   
   const total = roundTo(drinks.reduce((acc, drink) => acc + getUnits(drink), 0), 1);
   
@@ -24,6 +35,7 @@ export default function Home() {
     <>
       {drinks && drinks.map((drink, i) => <Drink key={i} drink={drink} />)}
       <p>{total} units in total.</p>
+      <button onClick={(e) => setDrinks([])}>Clear</button>
       
       <DrinkPicker addDrink={addDrink} />
     </>
