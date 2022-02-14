0eea34a7c7f5d7d7e71c15567244e2993e1e677c
Glitch (glitch-hello-react) <none>
2022-02-14 13:43:20 +0000

    🐞🎨 Checkpoint ./src/styles/styles.css:8338587/203 ./src/components/DrinkPicker.jsx:8338587/1064

--- src/components/DrinkPicker.jsx
+++ src/components/DrinkPicker.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import RadioGroup from './RadioGroup';
 
 export default function DrinkPicker({ addDrink }) {
-  const drinks = [ "Beer", "Wine", "Spirit" ];
+  const drinks = [ "Beer", "Wine", "Spirit", "Liqueur", "Low Alcohol Beer" ];
   const amounts = [ "25ml", "50ml", "125ml", "250ml", "Half Pint", "330ml", "Pint", "660ml" ];
   
   const [drink, setDrink] = useState(drinks[0]);
@@ -16,6 +16,28 @@ export default function DrinkPicker({ addDrink }) {
       amount: amount,
       percentage: percentage
     });
+  }
+  
+  let sliderMin = 0;
+  let sliderMax = 0;
+  let sliderStep = 0;
+  
+  if (drink === "Beer") {
+    sliderMin = 3;
+    sliderMax = 7;
+    sliderStep = 0.1;
+  } else if (drink === "Wine") {
+    sliderMin = 8;
+    sliderMax = 16;
+    sliderStep = 0.5;
+  } else if (drink === "Spirit") {
+    sliderMin = 10;
+    sliderMax = 50;
+    sliderStep = 1;
+  } else if (drink === "Liqueur") {
+    sliderMin = 8;
+    sliderMax = 20;
+    sliderStep = 1;
   }
   
   return (
@@ -31,9 +53,19 @@ export default function DrinkPicker({ addDrink }) {
         step="0.5"
         value={percentage}
         onChange={e => setPercentage(e.currentTarget.value)}
-        /><br />
+        />
       
-      <button onClick={onSubmit}>Add {amount} of {percentage}% {drink}</button>
+      <input
+        className="percentage-slider"
+        type="range"
+        min={sliderMin}
+        max={sliderMax}
+        step={sliderStep}
+        value={percentage}
+        onChange={e => setPercentage(e.currentTarget.value)}
+        />
+      
+      <button className="add-drink" onClick={onSubmit}>Add {amount} of {percentage}% {drink}</button>
     </div>
   )
 }
