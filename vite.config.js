7ce03b868be6a6af9dfd60ffc5ed4230c2849db1
Glitch (glitch-hello-react) <none>
2022-02-19 09:15:03 +0000

    🎥📈 Checkpoint ./vite.config.js:8338587/100 ./public/sw.js:8338587/91 ./index.html:8338587/161

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // reactRefresh(),
-    VitePWA()
+    VitePWA({
+      strategies: 'injectManifest',
+    })
   ],
   
   build: {
