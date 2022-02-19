314b940b29c7928e603932c6110d8cb5915b0e9c
Glitch (glitch-hello-react) <none>
2022-02-19 13:12:58 +0000

    🌜🌜 Checkpoint ./package.json:8338587/98 ./index.html:8338587/179 ./manifest.json:8338587/528 ./vite.config.js:8338587/594

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,31 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { VitePWA } from 'vite-plugin-pwa';
 
+const manifest = {
+  "short_name": "Drink Tracker",
+  "name": "Drink Tracker",
+  "icons": [
+    {
+      "src": "https://cdn.glitch.global/5ecd9d32-71f4-420b-ad21-eeaafcb59156/android-chrome-192x192.png?v=1644858005976",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "https://cdn.glitch.global/5ecd9d32-71f4-420b-ad21-eeaafcb59156/android-chrome-512x512.png?v=1644858009644",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "/",
+  "display": "standalone",
+  "theme_color": "#FFFFA0"
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     VitePWA({
-      strategies: 'injectManifest',
+      manifest: manifest
     })
   ],
   
