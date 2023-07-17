d4d18115d11de9894d484b6ced39a0807fe3089e
Peter E Conn <conn.peter@gmail.com>
2023-07-17 05:52:39 +0000

    Typescript-ified.

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -1,8 +1,8 @@
-export function roundTo(number, decimalPlaces) {
+export function roundTo(number: number, decimalPlaces: number): number {
   const power = 10 ** decimalPlaces;
   return Math.round(number * power) / power;
 }
 
-export function getFormattedDate() {
+export function getFormattedDate(): string {
   return new Date().toLocaleString('en-GB', {  weekday: 'short', month: 'short', day: 'numeric' });
 }
