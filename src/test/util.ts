b186d4bacc077734c5a421d89f055b2bdd244183
PEConn <conn.peter@gmail.com>
2023-07-31 07:29:09 +0000

    Update gh-pages to output generated at 2442819

--- src/test/util.ts
+++
@@ -1,7 +0,0 @@
-export function hasTextContent(text: string) {
-    return (content?: string, element?: Element | null) => {
-        if (!element || !element.textContent) return false;
-
-        return element.textContent.includes(text);
-    }
-}
